5eab4c9f9903dd5ae1100b26284bd1a22ac3251a
Gabriel Augusto <jvg32145@gmail.com>
2026-02-06 04:01:35 -0300

    feat: Conta objetos e objetos com buraco

--- GenericStuff.c
+++ GenericStuff.c
@@ -11,61 +11,202 @@ typedef struct {
 } Coordenada;
 
 typedef struct {
-    Coordenada dados[100000];
+    int linhas;
+    int colunas;
+    Pixel **pixels;
+} Imagem;
+
+typedef struct {
+    Coordenada *dados;
     int topo;
+    int capacidade;
 } Pilha;
 
-void pilha_init(Pilha *p){
+typedef struct {
+    Coordenada min;
+    Coordenada max;
+} InfoObjeto;
+
+void pilha_init(Pilha *p, int capacidade_inicial){
     p->topo = 0;
+    p->capacidade = capacidade_inicial;
+    p->dados = malloc(capacidade_inicial * sizeof(Coordenada));
 }
 
-void pilha_push(Pilha *p, int i, int j){
-    p->dados[p->topo].i = i;
-    p->dados[p->topo].j = j;
+void pilha_push(Pilha *p, Coordenada coord){
+    if(p->topo >= p->capacidade){
+        p->capacidade *=2;
+        p->dados = realloc(p->dados, p->capacidade * sizeof(Coordenada));
+    }
+    p->dados[p->topo].i = coord.i;
+    p->dados[p->topo].j = coord.j;
     p->topo++;
-}
-
-Coordenada pilha_pop(Pilha *p){
-    p->topo--;
-    return p->dados[p->topo];
 }
 
 int pilha_vazia(Pilha *p){
     return p->topo == 0;
 }
 
- int vizinhaca_8[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
- int vizinhaca_4[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+Coordenada pilha_pop(Pilha *p){
+    if(pilha_vazia(p)){
+        Coordenada invalida = {-1, -1};
+        return invalida;
+    }
 
-void rotular_objeto(int linhas, int colunas, Pixel imagem[linhas][colunas], int i, int j, int rotulo, int vizinhaca){
+    p->topo--;
+    return p->dados[p->topo];
+}
+
+void pilha_free(Pilha *p){
+    free(p->dados);
+    p->dados = NULL;
+    p->capacidade = 0;
+    p->topo = 0;
+}
+
+const int vizinhaca_8[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+const int vizinhaca_4[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+void rotular_objeto(Imagem *img, Coordenada coord, int rotulo, InfoObjeto *info){
 
     Pilha pilha;
-    pilha_init(&pilha);
+    pilha_init(&pilha, 128);
 
-    pilha_push(&pilha, i, j);
-    imagem[i][j].rotulo = rotulo;
+    pilha_push(&pilha, coord);
+    img->pixels[coord.i][coord.j].rotulo = rotulo;
+
+    info->min.i = coord.i;
+    info->max.i = coord.i;
+    info->min.j = coord.j;
+    info->max.j = coord.j;
 
     while(!pilha_vazia(&pilha)){
         Coordenada atual = pilha_pop(&pilha);
 
-        for(int k=0; k < vizinhaca; k++){
-            int vi,vj;
-            if(vizinhaca == 4){
-                vi = atual.i + vizinhaca_4[k][0];
-                vj = atual.j + vizinhaca_4[k][1];
-            } else {
-                vi = atual.i + vizinhaca_8[k][0];
-                vj = atual.j + vizinhaca_8[k][1];
-            }
+        if(atual.i < info->min.i) info->min.i = atual.i;
+        if(atual.i > info->max.i) info->max.i = atual.i;
+        if(atual.j < info->min.j) info->min.j = atual.j;
+        if(atual.j > info->max.j) info->max.j = atual.j;
 
-            if(vi < 0 || vi >= linhas || vj < 0 || vj >= colunas) continue;
+        for(int k=0; k < 8; k++){
+            Coordenada vizinho;
+            vizinho.i = atual.i + vizinhaca_8[k][0];
+            vizinho.j = atual.j + vizinhaca_8[k][1];
+            
+            if(vizinho.i < 0 || vizinho.i >= img->linhas || vizinho.j < 0 || vizinho.j >= img->colunas) continue;
 
-            if(imagem[vi][vj].valor == 1 && imagem[vi][vj].rotulo == 0){
-                imagem[vi][vj].rotulo = rotulo;
-                pilha_push(&pilha, vi, vj);
+            if(img->pixels[vizinho.i][vizinho.j].valor == 1 && img->pixels[vizinho.i][vizinho.j].rotulo == 0){
+                img->pixels[vizinho.i][vizinho.j].rotulo = rotulo;
+                pilha_push(&pilha, vizinho);
             }
         }
     }
+
+    pilha_free(&pilha);
+
+}
+
+int** extrair_objeto(Imagem *img, int rotulo, InfoObjeto info, int *altura_out, int *largura_out){
+    
+    *altura_out = info.max.i - info.min.i + 3;
+    *largura_out = info.max.j - info.min.j + 3;
+
+    int **obj = malloc(*altura_out * sizeof(int *));
+    for(int i = 0; i < *altura_out; i++){
+        obj[i] = calloc(*largura_out, sizeof(int));
+    }
+
+    for(int i = info.min.i; i <= info.max.i; i++){
+        for(int j = info.min.j; j <= info.max.j; j++){
+            if(img->pixels[i][j].rotulo == rotulo){
+                obj[i - info.min.i + 1][j - info.min.j + 1] = 1;
+            }
+        }
+    }
+
+    return obj;
+}
+
+void marcar_viz4(Imagem *img, Coordenada coord){
+
+    Pilha pilha;
+    pilha_init(&pilha, 128);
+    pilha_push(&pilha, coord);
+    img->pixels[coord.i][coord.j].valor = -1;
+
+    while(!pilha_vazia(&pilha)){
+        Coordenada atual = pilha_pop(&pilha);
+
+        for(int k = 0; k < 4; k++){
+            Coordenada vizinho;
+            vizinho.i = atual.i + vizinhaca_4[k][0];
+            vizinho.j = atual.j + vizinhaca_4[k][1];
+
+            if(vizinho.i < 0 || vizinho.i >= img->linhas || vizinho.j < 0 || vizinho.j >= img->colunas) continue;
+
+            if(img->pixels[vizinho.i][vizinho.j].valor == 1){
+                img->pixels[vizinho.i][vizinho.j].valor = -1;
+                pilha_push(&pilha, vizinho);
+            }
+
+        }
+
+    }
+
+    pilha_free(&pilha);
+
+}
+
+Imagem* criar_imagem(int linhas, int colunas) {
+    Imagem *img = malloc(sizeof(Imagem));
+    img->linhas = linhas;
+    img->colunas = colunas;
+
+    img->pixels = malloc(linhas * sizeof(Pixel*));
+    for(int i = 0; i < linhas; i++){
+        img->pixels[i] = malloc(colunas * sizeof(Pixel));
+    }
+
+    return img;
+}
+
+void liberar_imagem(Imagem *img){
+    for(int i = 0; i < img->linhas; i++){
+        free(img->pixels[i]);
+    }
+    free(img->pixels);
+    free(img);
+}
+
+
+int tem_buraco(int **obj_isolado, int altura_obj, int largura_obj){
+
+    Imagem *obj_invertido = criar_imagem(altura_obj, largura_obj);
+
+   
+    for(int i = 0; i < altura_obj; i++){
+        for(int j = 0; j < largura_obj; j ++){
+            obj_invertido->pixels[i][j].valor = (obj_isolado[i][j] == 1) ? 0 : 1;
+        }
+    }
+
+    int num_componentes = 0;
+
+    for(int i = 0; i < altura_obj; i++){
+        for(int j = 0; j < largura_obj; j++){
+            if(obj_invertido->pixels[i][j].valor == 1) {
+                Coordenada coord;
+                coord.i = i;
+                coord.j = j;
+                num_componentes++;
+                marcar_viz4(obj_invertido, coord);
+            }
+        }
+    }
+
+    liberar_imagem(obj_invertido);
+
+    return (num_componentes > 1);
 }
 
 int main(int argc, char *argv[]) {
@@ -97,17 +238,16 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    //Cria o array da imagem no c com flag de visitado e padding
-    Pixel imagem[linhas][colunas];
+    Imagem *imagem = criar_imagem(linhas, colunas);
 
     for(int i = 0; i < linhas; i ++){
         for(int j = 0; j < colunas; j++){
             if(i == 0 || j == 0 || i == linhas - 1 || j == colunas - 1){
-                imagem[i][j].valor = 0;
-                imagem[i][j].rotulo = 0;
+                imagem->pixels[i][j].valor = 0;
+                imagem->pixels[i][j].rotulo = 0;
 
-            } else if(fscanf(entrada, "%d", &imagem[i][j].valor) == 1) {
-               imagem[i][j].rotulo = 0;
+            } else if(fscanf(entrada, "%d", &imagem->pixels[i][j].valor) == 1) {
+               imagem->pixels[i][j].rotulo = 0;
             } else{
                 printf("Pixel não lido corretamente");
                 return 1;
@@ -119,25 +259,43 @@ int main(int argc, char *argv[]) {
 
     for(int q  = 0; q < linhas; q ++){
         for(int k = 0; k < colunas; k ++){
-            printf("%d ", imagem[q][k].valor);
+            printf("%d ", imagem->pixels[q][k].valor);
         }
         printf("\n");
     }
 
+    InfoObjeto objetos_info[1000];
     int num_objetos = 0;
-   
-    int va, vb;
 
     for(int a = 1; a < linhas - 1; a ++){
         for(int b = 1; b < colunas - 1; b ++){
-            if(imagem[a][b].valor == 1 && imagem[a][b].rotulo == 0){
+            if(imagem->pixels[a][b].valor == 1 && imagem->pixels[a][b].rotulo == 0){
                 num_objetos++;
-                rotular_objeto(linhas, colunas, imagem, a, b, num_objetos, 8);
+                Coordenada coord;
+                coord.i = a;
+                coord.j = b;
+                rotular_objeto(imagem, coord, num_objetos, &objetos_info[num_objetos - 1]);
             }
         }
     }
 
-    printf("Total de objetos: %d\n", num_objetos);
+    int objetos_com_buraco = 0;
+
+    for(int id = 1; id <= num_objetos; id++){
+        int altura_obj, largura_obj;
+        int **obj_isolado = extrair_objeto(imagem, id, objetos_info[id - 1], &altura_obj, &largura_obj);
+    
+        if(tem_buraco(obj_isolado, altura_obj, largura_obj)){
+            objetos_com_buraco++;
+        }
+
+        for(int i = 0; i < altura_obj; i++) free(obj_isolado[i]);
+        free(obj_isolado);
+    
+    }
+
+    printf("Objeto(s): %d\n", num_objetos);
+    printf("Objeto(s) com buraco: %d\n", objetos_com_buraco);
 
     return 0;
 }
